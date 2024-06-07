83d8e3d14d2a060097604db1322327d584d4818f
LaloG19 <21362134@utcancun.edu.mx>
2024-06-07 00:27:35 -0500

    fix: Corrección de ruta de producción de swagger

--- src/swagger.options.ts
+++ src/swagger.options.ts
@@ -1,22 +1,21 @@
 export const options = {
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'The API of The Road',
-        version: '1.0.0',
-        description: "Schoolar project 'The Road', using Express, TypeScript, and Swagger.",
-      },
-      servers: [
-        {
-            name: 'Desarrollo',
-            url: `http://localhost:${process.env.PORT ? process.env.PORT : 3000 }`,
-        },
-        {
-            name: 'Producción',
-            url: 'www.google.com',
-        }
-      ],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'The API of The Road',
+      version: '1.0.0',
+      description: "Schoolar project 'The Road', using Express, TypeScript, and Swagger.",
     },
-    apis: ['./src/routes/*.ts'],
-  };
-  
+    servers: [
+      {
+        name: 'Desarrollo',
+        url: `http://localhost:${process.env.PORT ? process.env.PORT : 3000 }`,
+      },
+      {
+        name: 'Producción',
+        url: 'https://the-road-api.vercel.app/',
+      }
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
