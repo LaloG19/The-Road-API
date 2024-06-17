77876b2d4e2122b7a0ed41eb26d0e8d1a76955be
Carloscastro16 <andycastro.001016@gmail.com>
2024-06-17 15:39:47 -0500

    Fix genre endpoints

---
+++ src/routes/genres.ts
@@ -0,0 +1,144 @@
+import { Router } from 'express';
+import * as genresController from '../controllers/Genres/controller';
+
+const genreRouter = Router();
+
+/**
+ * @swagger
+ * /api/genre/getAllGenres:
+ *   get:
+ *     summary: Obtener todos los usuarios
+ *     description: Retorna una lista de todos los usuarios
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
+genreRouter.get('/getAllGenres', genresController.GetAllGenresController);
+
+/**
+ * @swagger
+ * /api/genre/getGenreById/{id}:
+ *   get:
+ *     summary: Obtener usuario por ID
+ *     description: Retorna un usuario específico por su ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID del usuario
+ *     responses:
+ *       200:
+ *         description: Usuario encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
+genreRouter.get('/getGenreById/:id', genresController.GetGenreByIdController);
+
+/**
+ * @swagger
+ * /api/genre/createGenre:
+ *   post:
+ *     summary: Crear un nuevo usuario
+ *     description: Crea un nuevo usuario y lo guarda en la base de datos
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password: 
+ *                 type: string
+ *               rolename: 
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Usuario creado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
+genreRouter.post('/createGenre', genresController.CreateGenreController);
+
+/**
+ * @swagger
+ * /api/genre/updateGenreById/{id}:
+ *   put:
+ *     summary: Actualizar un usuario por ID
+ *     description: Actualiza los detalles de un usuario específico por su ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID del usuario
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password: 
+ *                 type: string
+ *               rolename: 
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Usuario actualizado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
+genreRouter.put('/updateGenreById/:id', genresController.UpdateGenreByIdController);
+
+
+/**
+ * @swagger
+ * /api/genre/deleteGenreById/{id}:
+ *   put:
+ *     summary: Actualizar un usuario por ID
+ *     description: Actualiza los detalles de un usuario específico por su ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID del usuario
+ *     responses:
+ *       200:
+ *         description: Usuario Eliminado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
+genreRouter.delete('/deleteGenreById/:id', genresController.DeleteGenreByIdController);
+ 
+export default genreRouter;
