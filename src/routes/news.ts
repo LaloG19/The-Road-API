a42297ebf104df9ccb430d9f12cdb21245f57990
LaloG19 <21362134@utcancun.edu.mx>
2024-07-14 15:45:19 -0500

    fix: Corrección de documentación (Swagger) en el módulo de Novedades

--- src/routes/news.ts
+++ src/routes/news.ts
@@ -18,9 +18,10 @@ const NewsRouter = Router();
  * @swagger
  * /api/news/getNews:
  *   get:
- *     summary: Obtiene todas las noticias
+ *     summary: Obtener todas las noticias
  *     tags:
- *      - Novedades
+ *       - Novedades
+ *     description: Retorna una lista de todas las noticias registradas
  *     responses:
  *       200:
  *         description: Lista de noticias
@@ -30,6 +31,16 @@ const NewsRouter = Router();
  *               type: array
  *               items:
  *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: "6671f8c5f066417aa3164939"
+ *                   title:
+ *                     type: string
+ *                     example: "Nueva actualización de software"
+ *                   content:
+ *                     type: string
+ *                     example: "Se ha lanzado una nueva versión con varias mejoras y correcciones de errores."
  */
 NewsRouter.get('/getNews', getAllNewsController);
 
@@ -39,7 +50,7 @@ NewsRouter.get('/getNews', getAllNewsController);
  *   get:
  *     summary: Obtiene una noticia por ID
  *     tags:
- *      - Novedades
+ *       - Novedades
  *     parameters:
  *       - in: path
  *         name: id
@@ -54,6 +65,25 @@ NewsRouter.get('/getNews', getAllNewsController);
  *           application/json:
  *             schema:
  *               type: object
+ *               properties:
+ *                 status:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: "Novedad Obtenida"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                       example: "669431775cef6c7497ecbe7f"
+ *                     title:
+ *                       type: string
+ *                       example: "Nueva actualización de software"
+ *                     content:
+ *                       type: string
+ *                       example: "Se ha lanzado una nueva versión con varias mejoras y correcciones de errores."
  */
 NewsRouter.get('/getNewsById/:id', getByIdController);
 
@@ -63,16 +93,40 @@ NewsRouter.get('/getNewsById/:id', getByIdController);
  *   post:
  *     summary: Crea una nueva noticia
  *     tags:
- *      - Novedades
+ *       - Novedades
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
  *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: "Nueva actualización de software"
+ *               content:
+ *                 type: string
+ *                 example: "Se ha lanzado una nueva versión con varias mejoras y correcciones de errores."
  *     responses:
  *       201:
  *         description: Noticia creada
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: integer
+ *                   example: 201
+ *                 message:
+ *                   type: string
+ *                   example: "Noticia creada con éxito"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                       example: "669431775cef6c7497ecbe7f"
  */
 NewsRouter.post('/CreateNews', CreateNewController);
 
@@ -82,12 +136,12 @@ NewsRouter.post('/CreateNews', CreateNewController);
  *   put:
  *     summary: Actualiza una noticia por ID
  *     tags:
- *      - Novedades
+ *       - Novedades
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
- *         description: ID de la noticia
+ *         description: ID de la noticia a actualizar
  *         schema:
  *           type: string
  *     requestBody:
@@ -96,29 +150,65 @@ NewsRouter.post('/CreateNews', CreateNewController);
  *         application/json:
  *           schema:
  *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: "Nueva actualización de software"
+ *               content:
+ *                 type: string
+ *                 example: "Se ha lanzado una nueva versión con varias mejoras y correcciones de errores."
  *     responses:
  *       200:
  *         description: Noticia actualizada
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: "Novedad actualizada con éxito"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                       example: "669431775cef6c7497ecbe7f"
+ *                     title:
+ *                       type: string
+ *                       example: "Nueva actualización de software"
+ *                     content:
+ *                       type: string
+ *                       example: "Se ha lanzado una nueva versión con varias mejoras y correcciones de errores."
  */
 NewsRouter.put('/UpdateNewsById/:id', updateNewsByIdController);
 
 /**
  * @swagger
  * /api/news/deleteNewsById/{id}:
- *   put:
+ *   delete:
  *     summary: Elimina una noticia por ID
  *     tags:
- *      - Novedades
+ *       - Novedades
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
- *         description: ID de la noticia
+ *         description: ID de la noticia a eliminar
  *         schema:
  *           type: string
  *     responses:
  *       200:
  *         description: Noticia eliminada
+ *         content:
+ *           application/json:
+ *             example:
+ *               status: 200
+ *               message: "Novedad Eliminada con éxito"
+ *               data: "6671f8c5f066417aa3164939"  # ID de la noticia eliminada
  */
 NewsRouter.delete('/deleteNewsById/:id', DeleteNewByIdController);
 
